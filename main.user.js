807c504a771731e7d8c7af06ffed79ad3b734635
嫣 <100054954+FairyXH@users.noreply.github.com>
2026-08-28 14:04:29 +0800

    Update

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         华夏系统增强工具
 // @namespace    hxxy-enhancer
-// @version      5.8.2
+// @version      5.8.3
 // @description  华夏系统增强工具
 // @author     	 Zhang
 // @license    	 MIT
